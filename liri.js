41af885c69490939ac651be84d4d6a26ec7b0d67
Adnan Niaz <adnanniaz77@yahoo.com>
2019-11-23 01:48:10 -0800

    install required packages and added them to the project

--- liri.js
+++ liri.js
@@ -1,2 +1,12 @@
 require("dotenv").config();
-var keys = require("./keys.js");
+let keys = require("./keys.js");
+let spotify = new spotify(keys.spotify);
+
+const fs = require('fs');
+const moment = require('moment');
+const chalk = require('chalk');
+const axios = require('axios');
+
+//"https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp"
+
+
