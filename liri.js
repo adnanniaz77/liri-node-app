8c2df46f6282276f7824a719b7b48c93f1ffcdb0
Adnan Niaz <adnanniaz77@yahoo.com>
2019-11-24 17:09:12 -0800

    getMoviesInfo is working now

--- liri.js
+++ liri.js
@@ -1,12 +1,59 @@
 require("dotenv").config();
 let keys = require("./keys.js");
-let spotify = new spotify(keys.spotify);
+// var spotify = new Spotify(keys.spotify);
 
 const fs = require('fs');
 const moment = require('moment');
 const chalk = require('chalk');
 const axios = require('axios');
 
-//"https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp"
 
 
+// //////////////////////////////
+// bandName = "kiss"
+// var queryURL = "https://rest.bandsintown.com/artists/" + bandName + "/events?app_id=[key]";
+// console.log(queryURL); 
+
+// axios.get(queryURL).then(
+//     function(bandResponse){
+//         console.log("Venue: " + bandResponse.data[0].venue.name);
+//         console.log("City: " + bandResponse.data[0].venue.city);
+//         console.log(moment(bandResponse.data[0].datetime).format("MM/DD/YYYY"));
+//     }
+// );
+
+
+// ///////////////////////////////
+// var spotify = new Spotify({
+//     id: spotifyKeyInfo["spotify"].id,
+//     secret: spotifyKeyInfo["spotify"].secret
+// });
+
+// spotify.request('https://api.spotify.com/v1/search?q=track:' + songName + '&type=track&limit=10', function(error, songResponse) {
+//     if (error){
+//         return console.log(error);
+//     }
+//     console.log("Artist: " + songResponse.tracks.items[0].artists[0].name);
+//     console.log("Song: " + songResponse.tracks.items[0].name);
+//     console.log("URL: " + songResponse.tracks.items[0].preview_url);
+//     console.log("Album: " + songResponse.tracks.items[0].album.name);
+// });
+
+function getMovieInfo(movieName) {
+    const apiKey = "7d74346a";
+    var queryURL = `http://www.omdbapi.com/?t=" + ${movieName} + "&y=&plot=short&apikey=${apiKey}`;
+
+    axios.get(queryURL).then(
+    function(movieResponse){
+        // console.log(movieResponse)
+        console.log("Title: " + movieResponse.data.Title);
+        console.log("Year: " + movieResponse.data.Year);
+        console.log("Rated: " + movieResponse.data.imdbRating);
+        console.log("Country: " + movieResponse.data.Country);
+        console.log("Language: " + movieResponse.data.Language);
+        console.log("Plot: " + movieResponse.data.Plot);
+        console.log("Actors: " + movieResponse.data.Actors);
+        console.log("Rotten Tomatoes: " + movieResponse.data.Ratings[1].Value);
+    }
+);
+}
